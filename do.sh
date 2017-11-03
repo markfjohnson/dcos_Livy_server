5b7ad3f111e18993f632e6b760d526863e43d980
markjohnson <markfjohnson@gmail.com>
2017-11-03 08:51:02 -0400

    updates

--- do.sh
+++ do.sh
@@ -1,3 +1,3 @@
-
-docker build -t markfjohnson:livy_spark .
-docker run -ti markfjohnson:livy_spark bash
+#mvn install:install-file -DgroupId=jdk.tools -DartifactId=jdk.tools -Dpackaging=jar -Dversion=1.8 -Dfile=tools.jar -DgeneratePom=truecd 
+docker build -f Spark_Docker -t markfjohnson:livy_spark .
+docker run  -ti markfjohnson:livy_spark bash
