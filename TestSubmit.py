e39396e26f334c46ee76a313f80a75f6c63cee3b
markjohnson <markfjohnson@gmail.com>
2017-11-12 19:21:16 -0500

    Cleanup checkin

--- TestSubmit.py
+++ TestSubmit.py
@@ -1,13 +1,13 @@
 import json, pprint, requests, textwrap, time, json
-host="http://34.215.224.225:10000"
+host="http://34.211.170.250:10000"
 
 # "file": "http://downloads.mesosphere.com/spark/assets/spark-examples_2.10-1.4.0-SNAPSHOT.jar",
 data = {
-    "name" : "SparkPi Livy Example 5 incubator1",
+    "name":"Spark-Pi-100",
     "file": "https://github.com/markfjohnson/dcos_Livy_server/raw/master/spark-examples_2.11-2.1.1.jar",
     "className" : "org.apache.spark.examples.SparkPi",
-    "executorMemory": "20g",
-    "args": ["100"],
+    "executorMemory": "2g",
+    "args": ["10"],
     "conf":{
         "spark.mesos.executor.docker.image":"mesosphere/spark:2.1.0-2.2.0-1-hadoop-2.6",
         "spark.logConf":"true",
